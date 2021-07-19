c110f1df39a9955b8b72d5ae537593bc41842158
radityarmd <radityaputra599@gmail.com>
2021-07-20 01:45:04 +0700

    baru lagi

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("awokakwok phising :v")
+alert('awokakwok phising :v')
+var nama = prompt('namamu?')
+alert("Sugeng enjing/siyang/dalu mas " + nama)
+var lagi = true
+while(lagi === true) {
+    var pernyataan = prompt("ketik 1 jika ingin nikah sama kak dapa")
+    if(pernyataan === '1') {
+        alert('mff, kak Daoa hanya milik Ruslan tercinta')
+    }
+    else {
+        alert("ketik 1 ngento-d")
+    }
+    lagi = confirm("mw jwb lg kh?")
+}
