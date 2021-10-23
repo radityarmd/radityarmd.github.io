a5ceb490f9834b22e081a7214e088986fe615f22
radityarmd <radityaputra599@gmail.com>
2021-10-23 23:12:08 +0700

    p

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-alert('awokakwok phising :v')
-var nama = prompt('namamu?')
-alert("halo  " + nama)
-var lagi = true
-while(lagi === true) {
-    var pernyataan = prompt("ketik 1 jika ingin masuk neraka")
-    if(pernyataan === '1') {
-        alert('أستغفر الله')
-    }
-    else {
-        alert("cuma ada 1 opsi😎")
-    }
-    lagi = confirm("ulangi?")
-}
+// alert('awokakwok phising :v')
+// var nama = prompt('namamu?')
+// alert("halo  " + nama)
+// var lagi = true
+// while(lagi === true) {
+//     var pernyataan = prompt("ketik 1 jika ingin masuk neraka")
+//     if(pernyataan === '1') {
+//         alert('أستغفر الله')
+//     }
+//     else {
+//         alert("cuma ada 1 opsi😎")
+//     }
+//     lagi = confirm("ulangi?")
+// }
